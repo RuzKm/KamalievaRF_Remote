225e47195805a090ed915014443edff1fd5924d3
KamalievaRF <rruzzi@yandex.ru>
2025-05-28 16:53:16 +0300

    Добавьте файлы проекта.

---
+++ KamalievaRF/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KamalievaRF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
